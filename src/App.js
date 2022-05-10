27a8c9fef62e35defcbb219be0a820afe282d56c
Amanda Woods <Amanda@Amandas-MacBook-Pro.local>
2022-05-10 14:08:33 -0700

    Home page and navbar up and running. NavBar should have full functionality once it has access to the controllers. Only thing is I can't get the image to display.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './views/NavBar';
+import Main from './views/Main'
+import Footer from './views/Footer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Main />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+//D86FA3 text color
+//fcf0cc background
+//100406 accent
