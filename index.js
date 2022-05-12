b495333a383ed58b6d0c1700435b7c53622d9d22
LuckyShmucky <christianluciano567@gmail.com>
2022-05-11 18:07:04 -0700

    connection string set up

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 //Dependencies
+require('dotenv').config()
 const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
 
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+    function(){ console.log('connected to mongoDB ') }
+  )
+
 // CONFIGURATION / MIDDLEWARE 
-require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use('/districts', require('./controllers/DistrictController'))
--- models/Districts.js
+++ models/Districts.js
@@ -3,11 +3,20 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 const {Schema } = mongoose
 
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopolgy: true
+})
+
 const districtSchema = new Schema ({
     name: {
         type: String,
         required: true
-    }
+    },
+    image: {type: String},
+    city: {type: String, required: true},
+    state: {type: String, required: true},
+     
 })
 
 
